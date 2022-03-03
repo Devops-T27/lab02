8b1b37e5a98fa23e466427250967fde25e1b153a
Devops-T27 <89909793+Devops-T27@users.noreply.github.com>
2022-03-03 07:04:14 -0500

    correct

--- lab02.py
+++ lab02.py
@@ -3,7 +3,15 @@ email = input("email please: ")
 password = input("password: ")
 
 def login(email,password):
-    pass
+     f = open("users.txt","r", encoding="utf-8")
+     for ligne in f:
+        list=ligne.split()
+        eml=list[1]
+        pas=list[2]
+        if eml == email and pas == password:
+            return True
+    f.close()
+    return False
         
 
 if login(email,password):
