4301bc37a88a0eed69335ba84d4a831770c8d654
Devops-T27 <devops-t27@zmwarelab.com>
2022-03-03 02:52:16 -0500

    test version

--- lab02.py
+++ lab02.py
@@ -1,1 +1,18 @@
+print("Login")
+email = input("email please: ")
+password = input("password: ")
 
+def login(email,password):
+    f = open("users.txt","r")
+    for ligne in f:
+        user = ligne.split()
+        if user[1]==email and user[2]==password:
+            return True
+    f.close()
+    return False
+        
+
+if login(email,password):
+    print("you logged in")
+else:
+    print("your email or password is incorrect")
